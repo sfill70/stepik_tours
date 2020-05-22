70017eda1da889ca338b5b90d0218440026d72c3
sfill70 <sergfill70@yandex.ru>
2020-05-22 13:00:44 +0300

    init stepik_tours

---
+++ tours/urls.py
@@ -0,0 +1,29 @@
+"""stepik_tours URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+from . views import custom_handler404, custom_handler500
+
+handler404 = custom_handler404
+handler500 = custom_handler500
+app_name = 'tours'
+
+urlpatterns = [
+    path('', views.MainView.as_view(), name='home'),
+    path('departure/<str:departure>/', views.DepartureView.as_view(), name='departure'),
+    path('tour/<int:id>/', views.TourView.as_view(), name='tour'),
+]
---
+++ tours/views.py
@@ -0,0 +1,104 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, View
+from tours import data
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+import random
+
+
+# Create your views here.
+
+def get_town(departure):
+    town = ''
+    if departure == 'msk':
+        town = "Москвы"
+    elif departure == 'spb':
+        town = "Питера"
+    elif departure == 'kazan':
+        town = "Казани"
+    elif departure == 'nsk':
+        town = "Новосибирска"
+    elif departure == 'ekb':
+        town = "Екатеринбурга"
+    return town
+
+
+def custom_handler404(request, exception=None):
+    return HttpResponseNotFound('Ой, что то сломалось... !')
+
+
+def custom_handler500(request, exception=None):
+    return HttpResponseNotFound('Ой, что то сломалось... !')
+
+
+class MainView(View):
+
+    # paginate_by = 6
+
+    def get(self, request, *args, **kwargs):
+        tours = {i: data.tours[i] for i in
+                 random.sample(range(1, len(data.tours)), 6)}
+        # tours = data.tours
+        context = {
+            'tours': tours,
+        }
+        return render(request, 'index.html', context)
+
+
+class DepartureView(View):
+
+    def get(self, request, departure, *args, **kwargs):
+
+        set_departure = {value.get('departure') for (key, value) in data.tours.items()}
+        if departure not in set_departure:
+            # raise Http404
+            return custom_handler404(request)
+
+        tours = {key: value for (key, value) in data.tours.items()
+                 if value['departure'] == departure}
+
+        town = get_town(departure)
+        array_price = []
+        array_nights = []
+
+        for keys, value in tours.items():
+            array_price.append(value['price'])
+            array_nights.append(value['nights'])
+
+        min_price = min(array_price)
+        max_price = max(array_price)
+        min_nights = min(array_nights)
+        max_nights = max(array_nights)
+        count_tours = len(array_price)
+
+        context = {
+            'tours': tours,
+            'town': town,
+            'min_price': min_price,
+            'max_price': max_price,
+            'min_nights': min_nights,
+            'max_nights': max_nights,
+            'count_tours': count_tours
+        }
+
+        return render(request, 'departure.html', context)
+
+
+class TourView(View):
+    def get(self, request, id):
+        if id not in data.tours:
+            return HttpResponseNotFound('Ой, что то сломалось... !')
+            # raise Http404
+            # custom_handler404(request)
+            # custom_handler500(request)
+
+        tour = data.tours[id]
+        departure = data.tours[id]["departure"]
+
+        town = get_town(departure)
+
+        print(town)
+
+        context = {'tour': tour,
+                   'departure': departure,
+                   'town': town}
+        return render(request, 'tour.html', context)
